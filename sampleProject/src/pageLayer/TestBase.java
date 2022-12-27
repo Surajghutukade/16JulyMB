429fa3dc26f16e2c19ea08345fba0435ac624153
Surajghutukade <surajghutukade16896@gmail.com>
2022-12-27 14:26:16 +0530

    Basic Commit
    hi guys,
    i have created project on github desktop go ahead and clone it and complete your task

---
+++ sampleProject/src/pageLayer/TestBase.java
@@ -0,0 +1,13 @@
+package pageLayer;
+
+public class TestBase {
+	public static void main(String[]args)
+	{
+		System.out.println("Hello World");
+		System.out.println("github.com");
+		System.out.println("Finish Work");
+		System.out.println("Added dependencies in pom.xml");
+		
+	}
+
+}
